fc06caf259c6b4254f80b33c45293d507f96bbe1
Milad Amiri <miladmarketing282.com>
2022-09-19 15:49:11 +0800

    added

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import data from "./data/data.js";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("This is home page")
+});
+
+app.get("/api/products", (req, res) => {
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Port is running in port ${PORT}`);
+})
